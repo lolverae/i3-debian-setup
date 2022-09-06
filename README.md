c95bcd338f975e79f55899a0147fc04c4cf932eb
alberto <alberto@skylance.bwrouter>
2022-09-05 21:03:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# i3gaps
+
+```
+sudo apt install git
+
+git clone https://github.com/lolverae/i3-debian-setup
+
+cd i3-debian-setup
+
+./install.sh (includes nerdfonts.sh and copyconf.sh unless commented)
+```
+
+OPTIONAL:
+Nerd Fonts installed by default but can be commented out of install.sh
+```
+# source ~/i3-debian-setup/nerdfonts.sh
+```
+OPTIONAL:
+Customized configuration installed by default but can be commented out of install.sh
+```
+# source ~/i3-debian-setup/copyconf.sh
+```
+NOT OPTIONAL
+```
+sudo reboot
+```
---
+++ install.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Default packages are for the configuration and corresponding .config folders
+# Install packages after installing base Debian with no GUI
+sudo apt update
+
+# xorg display server installation
+sudo apt install -y xorg 
+
+# Python installed for bumblebee-status. PACKAGE INCLUDES build-essential.
+sudo apt install -y python3-pip 
+
+# Microcode for Intel
+sudo apt install -y intel-microcode 
+
+# Network Manager
+sudo apt install -y network-manager-gnome
+
+# Installation for Appearance management
+sudo apt install -y lxappearance 
+
+# File Manager
+sudo apt install -y thunar xfce4-settings ranger
+
+# Terminal
+sudo apt install -y kitty
+
+# Sound
+sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+
+# Neofetch/HTOP
+sudo apt install -y neofetch htop
+
+# exa installation
+sudo apt install -y exa
+
+# BatCat installation
+sudo apt install -y bat
+
+# Github CLI
+sudo apt install -y gh
+
+# Zathura
+sudo apt install -y zathura
+# ZSH and oh-my-zsh + powerlevel
+sudo apt install -y zsh curl
+sudo chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Neovim and nvchad
+sudo apt install -y neovim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
+# RipGrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
+# Browser Installation (eg. chromium)
+sudo apt install -y firefox-esr 
+
+# Desktop background browser/handler 
+sudo apt install -y feh
+
+# Required packages for i3-gaps installation
+sudo apt install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev i3status
+
+# Packages needed i3-gaps after installation
+sudo apt install -y dmenu sxhkd numlockx rofi dunst libnotify-bin picom unzip geany simple-scan
+
+# Install fonts
+sudo apt install fonts-font-awesome fonts-powerline fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus fonts-cascadia-code
+
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
+
+# Install i3 gaps
+cd
+cd Downloads
+git clone https://github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+mkdir -p build && cd build
+meson --prefix /usr/local
+ninja
+sudo ninja install
+
+# Dependencies for Ly Console Manager
+sudo apt install -y libpam0g-dev libxcb-xkb-dev
+
+# Install Ly Console Display Manager
+cd 
+cd Downloads
+git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly/
+make
+make run
+make install installsystemd
+systemctl enable ly.service
+
+# XSessions and i3.desktop
+if [[ ! -d /usr/share/xsessions ]]; then
+    sudo mkdir /usr/share/xsessions
+fi
+
+cat > ./temp << "EOF"
+[Desktop Entry]
+Encoding=UTF-8
+Name=i3
+Comment=Dynamic window manager
+Exec=i3
+Icon=i3
+Type=XSession
+EOF
+sudo cp ./temp /usr/share/xsessions/i3.desktop;rm ./temp
+
+########################################################
+# End of script for default config
+#
+
+## These two scripts will install nerdfonts and copy my configuration files into the ~/.config directory
+## Configuration uses 
+
+source ~/i3gaps-debian/nerdfonts.sh
+source ~/i3gaps-debian/copyconf.sh
+
+sudo apt upgrade
+sudo apt autoremove
+
+printf "\e[1;32mYou can now reboot! Thanks you.\e[0m\n"
